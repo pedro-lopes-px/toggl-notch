624fd48a905fa2723106791681d2127fb4e5ed02
pedro-lopes-px <pedro-lopes-px@users.noreply.github.com>
2026-06-12 18:55:54 +0100

    Initial Commit

---
+++ toggl-notch/toggl_notchApp.swift
@@ -0,0 +1,17 @@
+//
+//  toggl_notchApp.swift
+//  toggl-notch
+//
+//  Created by Pedro Lopes on 12/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct toggl_notchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
